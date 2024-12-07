6b5b26fb39777ea6ff58e9e874266c87d1f7c668
Đang Nam <dangnam1303k3@gmail.com>
2024-12-07 20:32:25 +0700

    fish commit

---
+++ ss7/practices/script.js
@@ -0,0 +1,7 @@
+let width = parseInt(prompt("Nhập giá trị chiều ngang"))  
+let height = parseInt(prompt("Nhập giá trị chiều cao"));
+
+let area = width * height 
+
+document.write("the area: " + area)
+
